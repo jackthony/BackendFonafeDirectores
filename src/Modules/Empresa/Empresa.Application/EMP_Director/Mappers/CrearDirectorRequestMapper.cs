65ee98635d2e22f37ea02faa3783fe9ada46598a
JackAcity <jaaguilar@acity.com.pe>
2025-06-22 19:55:38 -0500

    crear y eliminar request y mapper de director

--- src/Modules/Empresa/Empresa.Application/EMP_Director/Mappers/CrearDirectorRequestMapper.cs
+++ src/Modules/Empresa/Empresa.Application/EMP_Director/Mappers/CrearDirectorRequestMapper.cs
@@ -18,7 +18,35 @@ namespace Empresa.Application.Director.Mappers
         {
             return new CrearDirectorParameters
             {
-                
+                nEmpresaId = source.nEmpresaId,
+                nTipoDocumentoId = source.nTipoDocumentoId,
+                sNumeroDocumento = source.sNumeroDocumento,
+                sNombres = source.sNombres,
+                sApellidos = source.sApellidos,
+                dtFechaNacimiento = source.dtFechaNacimiento,
+                nGeneroId = source.nGeneroId,
+                nDistritoId = source.nDistritoId,
+                nProvinciaId = source.nProvinciaId,
+                nDepartamentoId = source.nDepartamentoId,
+                sDireccion = source.sDireccion,
+                sTelefono = source.sTelefono,
+                sTelefonoSecundario = source.sTelefonoSecundario,
+                sTelefonoTerciario = source.sTelefonoTerciario,
+                sCorreo = source.sCorreo,
+                sCorreoSecundario = source.sCorreoSecundario,
+                sCorreoTerciario = source.sCorreoTerciario,
+                nCargoId = source.nCargoId,
+                nTipoDirectorId = source.nTipoDirectorId,
+                nSectorId = source.nSectorId,
+                sProfesion = source.sProfesion,
+                dDieta = source.dDieta,
+                nEspecialidadId = source.nEspecialidadId,
+                dtFechaNombramiento = source.dtFechaNombramiento,
+                dtFechaDesignacion = source.dtFechaDesignacion,
+                dtFechaRenuncia = source.dtFechaRenuncia,
+                sComentario = source.sComentario,
+                dtFechaRegistro = _timeProvider.NowPeru,
+                nUsuarioRegistroId = source.nUsuarioRegistroId
             };
         }
     }
--- src/Modules/Empresa/Empresa.Application/EMP_Director/Mappers/EliminarDirectorRequestMapper.cs
+++ src/Modules/Empresa/Empresa.Application/EMP_Director/Mappers/EliminarDirectorRequestMapper.cs
@@ -1,14 +1,26 @@
 ﻿using Shared.Kernel.Interfaces;
 using Empresa.Application.Director.Dtos;
 using Empresa.Domain.Director.Parameters;
+using Shared.Time;
 
 namespace Empresa.Application.Director.Mappers
 {
     public class EliminarDirectorRequestMapper : IMapper<EliminarDirectorRequest, EliminarDirectorParameters>
     {
+        private readonly ITimeProvider _timeProvider;
+
+        public EliminarDirectorRequestMapper(ITimeProvider timeProvider)
+        {
+            _timeProvider = timeProvider;
+        }
         public EliminarDirectorParameters Map(EliminarDirectorRequest source)
         {
-            throw new NotImplementedException();
+            return new EliminarDirectorParameters
+            {
+                nDirectorId = source.nDirectorId,
+                dtFechaModificacion = source.dtFechaModificacion,
+                nUsuarioModificacionId = source.nUsuarioModificacionId
+            };
         }
     }
 }
