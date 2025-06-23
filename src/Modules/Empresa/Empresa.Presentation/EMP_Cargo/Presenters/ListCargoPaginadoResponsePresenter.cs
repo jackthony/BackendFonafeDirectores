11f09bf8e597b05122b9b0216c2ec4d5aa22575b
Alexis G <iginio99gamboa@gmail.com>
2025-06-23 01:51:54 -0500

    add indice list

--- src/Modules/Empresa/Empresa.Presentation/EMP_Cargo/Presenters/ListCargoPaginadoResponsePresenter.cs
+++ src/Modules/Empresa/Empresa.Presentation/EMP_Cargo/Presenters/ListCargoPaginadoResponsePresenter.cs
@@ -11,9 +11,17 @@ namespace Empresa.Presentation.Cargo.Presenters
     {
         public LstItemResponse<CargoResponse> Present(PagedResult<CargoResult> input)
         {
+            int startIndex = (input.Page - 1) * input.PageSize;
+
+            var lista = CargoResponseMapper.ToListResponse(input.Items);
+            var listaItems = lista.ToList();
+            for (int i = 0; i < listaItems.Count; i++)
+            {
+                listaItems[i].indice = startIndex + i + 1;
+            }
             return new LstItemResponse<CargoResponse>
             {
-                LstItem = CargoResponseMapper.ToListResponse(input.Items),
+                LstItem = lista,
                 Pagination = new Pagination
                 {
                     PageIndex = input.Page,
--- src/Modules/Empresa/Empresa.Presentation/EMP_Sector/Presenters/ListSectorPaginadoResponsePresenter.cs
+++ src/Modules/Empresa/Empresa.Presentation/EMP_Sector/Presenters/ListSectorPaginadoResponsePresenter.cs
@@ -11,9 +11,17 @@ namespace Empresa.Presentation.Sector.Presenters
     {
         public LstItemResponse<SectorResponse> Present(PagedResult<SectorResult> input)
         {
+            int startIndex = (input.Page - 1) * input.PageSize;
+
+            var lista = SectorResponseMapper.ToListResponse(input.Items);
+            var listaItems = lista.ToList();
+            for (int i = 0; i < listaItems.Count; i++)
+            {
+                listaItems[i].indice = startIndex + i + 1;
+            }
             return new LstItemResponse<SectorResponse>
             {
-                LstItem = SectorResponseMapper.ToListResponse(input.Items),
+                LstItem = lista,
                 Pagination = new Pagination
                 {
                     PageIndex = input.Page,
