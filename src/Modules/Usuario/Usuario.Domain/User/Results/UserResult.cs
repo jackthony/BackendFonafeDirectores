13e0610dc2116b66ecb3bbe2438038e10f5e331a
Alexis G <iginio99gamboa@gmail.com>
2025-06-24 14:22:00 -0500

    user modified

--- src/Modules/Usuario/Usuario.Domain/User/Results/UserResult.cs
+++ src/Modules/Usuario/Usuario.Domain/User/Results/UserResult.cs
@@ -2,13 +2,20 @@
 {
     public class UserResult
     {
-        public int UsuarioId { get; set; }
-        public string ApellidosYNombres { get; set; } = string.Empty;
-        public string Cargo { get; set; } = string.Empty;
-        public string Perfil { get; set; } = string.Empty;
-        public bool Estado { get; set; }
-        public DateTime FechaCreacion { get; set; }
-        public DateTime? UltimaModificacion { get; set; }
-        public string Correo { get; set; } = string.Empty;
+        public int IdUsuario { get; set; }
+        public string ApellidoPaterno { get; set; } = string.Empty;
+        public string ApellidoMaterno { get; set; } = string.Empty;
+        public string Nombres { get; set; } = string.Empty;
+        public int IdCargo { get; set; }
+        public int IdRol { get; set; }
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public int Estado { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public int UsuarioRegistro { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        public int? UsuarioModificacion { get; set; }
+        public string? CargoDescripcion { get; set; }
+        public string? PerfilDescripcion { get; set; }
+        public string? EstadoDescripcion { get; set; }
     }
 }
