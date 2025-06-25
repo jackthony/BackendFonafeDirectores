c83e2d39074682f60d9d6ae80474c6b3fdf72906
Alexis G <iginio99gamboa@gmail.com>
2025-06-24 19:13:30 -0500

    actualizar campos de requestdirectoractualizar

--- src/Modules/Empresa/Empresa.Application/EMP_Director/Dtos/ActualizarDirectorRequest.cs
+++ src/Modules/Empresa/Empresa.Application/EMP_Director/Dtos/ActualizarDirectorRequest.cs
@@ -4,33 +4,25 @@
     {
         public int nIdRegistro { get; set; }
         public int nIdEmpresa { get; set; }
-        public int? nTipoDocumentoId { get; set; }
-        public string? sNumeroDocumento { get; set; } = string.Empty;
-        public string? sNombres { get; set; } = string.Empty;
-        public string? sApellidos { get; set; } = string.Empty;
-        public DateTime? dtFechaNacimiento { get; set; }
-        public int? nGeneroId { get; set; }
-        public int? nDistritoId { get; set; }
-        public int? nProvinciaId { get; set; }
-        public int? nDepartamentoId { get; set; }
-        public string? sDireccion { get; set; } = string.Empty;
-        public string? sTelefono { get; set; } = string.Empty;
-        public string? sTelefonoSecundario { get; set; } = string.Empty;
-        public string? sTelefonoTerciario { get; set; } = string.Empty;
-        public string? sCorreo { get; set; } = string.Empty;
-        public string? sCorreoSecundario { get; set; } = string.Empty;
-        public string? sCorreoTerciario { get; set; } = string.Empty;
-        public int? nCargoId { get; set; }
-        public int? nTipoDirectorId { get; set; }
-        public int? nIdSector { get; set; }
-        public string? sProfesion { get; set; } = string.Empty;
-        public decimal? dDieta { get; set; }
-        public int? nEspecialidadId { get; set; }
-        public DateTime? dtFechaNombramiento { get; set; }
-        public DateTime? dtFechaDesignacion { get; set; }
-        public DateTime? dtFechaRenuncia { get; set; }
-        public string? sComentario { get; set; } = string.Empty;
-        public DateTime? dtFechaModificacion { get; set; }
-        public int? nUsuarioModificacionId { get; set; }
+        public string sDepartamento { get; set; }
+        public string sProvincia { get; set; }
+        public string sDistrito { get; set; }
+        public string sDireccion { get; set; }
+        public string sTelefono { get; set; }
+        public string sTelefonoSecundario { get; set; }
+        public string sTelefonoTerciario { get; set; }
+        public string sCorreo { get; set; }
+        public string sCorreoSecundario { get; set; }
+        public string sCorreoTerciario { get; set; }
+        public int nCargo { get; set; }
+        public int nTipoDirector { get; set; }
+        public string sProfesion { get; set; }
+        public int nIdSector { get; set; }
+        public decimal mDieta { get; set; }
+        public int nEspecialidad { get; set; }
+        public DateTime dFechaNombramiento { get; set; }
+        public DateTime dFechaDesignacion { get; set; }
+        public string sComentario { get; set; }
+        public int nUsuarioModificacion { get; set; }
     }
 }
--- src/Modules/Empresa/Empresa.Domain/EMP_Director/Parameters/ActualizarDirectorParameters.cs
+++ src/Modules/Empresa/Empresa.Domain/EMP_Director/Parameters/ActualizarDirectorParameters.cs
@@ -4,15 +4,9 @@
     {
         public int nDirectorId { get; set; }
         public int nEmpresaId { get; set; }
-        public int? nTipoDocumentoId { get; set; } // Nullable para que no sea necesario actualizarlo siempre
-        public string? sNumeroDocumento { get; set; } = string.Empty;
-        public string? sNombres { get; set; } = string.Empty;
-        public string? sApellidos { get; set; } = string.Empty;
-        public DateTime? dtFechaNacimiento { get; set; } // Nullable para poder no actualizar la fecha de nacimiento
-        public int? nGeneroId { get; set; }
-        public int? nDistritoId { get; set; }
-        public int? nProvinciaId { get; set; }
         public int? nDepartamentoId { get; set; }
+        public int? nProvinciaId { get; set; }
+        public int? nDistritoId { get; set; }
         public string? sDireccion { get; set; } = string.Empty;
         public string? sTelefono { get; set; } = string.Empty;
         public string? sTelefonoSecundario { get; set; } = string.Empty;
@@ -24,13 +18,12 @@
         public int? nTipoDirectorId { get; set; }
         public int? nSectorId { get; set; }
         public string? sProfesion { get; set; } = string.Empty;
-        public decimal? dDieta { get; set; } // Nullable para poder no actualizar la dieta
+        public decimal? dDieta { get; set; }
         public int? nEspecialidadId { get; set; }
-        public DateTime? dtFechaNombramiento { get; set; } // Nullable
-        public DateTime? dtFechaDesignacion { get; set; } // Nullable
-        public DateTime? dtFechaRenuncia { get; set; } // Nullable para el caso que no se renuncie
+        public DateTime? dtFechaNombramiento { get; set; }
+        public DateTime? dtFechaDesignacion { get; set; }
         public string? sComentario { get; set; } = string.Empty;
-        public DateTime? dtFechaModificacion { get; set; } // Fecha de la modificación
-        public int? nUsuarioModificacionId { get; set; } // El usuario que realiza la modificación
+        public DateTime? dtFechaModificacion { get; set; }
+        public int? nUsuarioModificacionId { get; set; }
     }
 }
