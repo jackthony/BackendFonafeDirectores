5d6d6901610ffc6d637e797359098ab96a347b59
Alexis G <iginio99gamboa@gmail.com>
2025-06-30 17:10:34 -0500

    recovery account start

---
+++ src/Modules/Usuario/Usuario.Application/Auth/Dtos/RecoveryAccountRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Usuario.Application.Auth.Dtos
+{
+    public class RecoveryAccountRequest
+    {
+        public required string sCorreo {  get; set; }
+        public required string sNombre { get; set; }
+        public required string sComentario { get; set; } = string.Empty;
+    }
+}
--- src/Shared/Kernel/Errors/ValidationProblemDetailsOur.cs
+++ src/Shared/Kernel/Errors/ValidationProblemDetailsOur.cs
@@ -1,18 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-
-namespace Shared.Kernel.Errors
+﻿namespace Shared.Kernel.Errors
 {
-    public class ValidationProblemDetailsOur : ValidationProblemDetails
+    public class ValidationProblemDetailsOur : ProblemDetailsOur
     {
-        public bool ShowToast { get; set; } = true;
+        public IDictionary<string, string[]> Errors { get; } = new Dictionary<string, string[]>(StringComparer.Ordinal);
 
-        public ValidationProblemDetailsOur() : base()
+        public ValidationProblemDetailsOur()
         {
         }
 
-        public ValidationProblemDetailsOur(IDictionary<string, string[]> errors) : base()
+        public ValidationProblemDetailsOur(IDictionary<string, string[]> errors)
         {
             if (errors == null)
                 throw new ArgumentNullException(nameof(errors));
