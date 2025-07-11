41fc9501be382d5b488f9e19419da55ea07b65b4
Alexis G <iginio99gamboa@gmail.com>
2025-07-10 21:32:58 -0500

    correccion rubro

--- src/Modules/Empresa/Empresa.Application/EMP_Rubro/Mappers/ListarRubroPaginadoRequestMapper.cs
+++ src/Modules/Empresa/Empresa.Application/EMP_Rubro/Mappers/ListarRubroPaginadoRequestMapper.cs
@@ -28,24 +28,3 @@ namespace Empresa.Application.Rubro.Mappers
         }
     }
 }
-
-using Shared.Kernel.Interfaces;
-using Empresa.Application.Rubro.Dtos;
-using Empresa.Domain.Rubro.Parameters;
-
-namespace Empresa.Application.Rubro.Mappers
-{
-    public class ListarRubroPaginadoRequestMapper : IMapper<ListarRubroPaginadoRequest, ListarRubroPaginadoParameters>
-    {
-        public ListarRubroPaginadoParameters Map(ListarRubroPaginadoRequest source)
-        {
-            return new ListarRubroPaginadoParameters
-            {
-                Page = source.Page,
-                PageSize = source.PageSize,
-                Estado = source.Estado,
-                Nombre = source.Nombre,
-            };
-        }
-    }
-}
